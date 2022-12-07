86943fef02f7c08211694de36c4802f7dfc0dcc4
Mustafaciftci24 <mustafaciftci024@outlook.com>
2022-12-07 20:40:15 +0300

    login created

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
-#New Comment
+
 @app.route("/")
-def hello():
-    return "Hello World"
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
