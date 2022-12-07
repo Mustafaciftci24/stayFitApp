7530b9ab3f5c39cb8b4af066b51408543c0c6064
Mustafaciftci24 <mustafaciftci024@outlook.com>
2022-12-08 01:31:21 +0300

    Database Connected

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 from flask import Flask, render_template
+import psycopg2
 app = Flask(__name__)
+
+
+def get_db_connection():
+    conn = psycopg2.connect(host='localhost',
+                            database='postgres',
+                            user="postgres",
+                            password="Mustafa2323")
+    return conn
 
 
 @app.route("/")
@@ -10,5 +19,11 @@ def login():
 
 @app.route("/register")
 def register():
-    return render_template("register.html")
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT * from \"Company\".\"Department\"")
+    books = cur.fetchall()
+    cur.close()
+    conn.close()
+    return render_template("register.html", books=books )
 
